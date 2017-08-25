df30ccefeec6d949666415249c5f7f3506385778
Jasmine Quach <xjasminequach@gmail.com>
2017-08-24 20:35:20 -0500

    Add cast call feature

--- javascripts/db-interaction.js
+++ javascripts/db-interaction.js
@@ -15,14 +15,13 @@ let $ = require('jquery'),
 let userInput = "";
 let apiLink = "https://api.themoviedb.org/3/search/movie?api_key=dbe82c339d871418f3be9db2647bb249&language=en-US&query=";
 
-function getApiMovies() {
+function getApiMovies() { //param with cast
     return new Promise((resolve, reject) => {
-
 
         userInput = $("#dbSearch").val();
         console.log("user input", userInput);
         $.ajax({
-            url: apiLink + userInput + "&page=1",
+            url: apiLink + userInput + "&page=1", //add cast here
             type: "GET",
             dataType: "json"
         }).done((response) => {
@@ -33,5 +32,28 @@ function getApiMovies() {
     });
 }
 
-module.exports = {getApiMovies};
+//second call with cast2
+
+let castLink = "https://api.themoviedb.org/3/movie/";
+let movieId = "";
+let apiKeyEnd = "/credits?api_key=dbe82c339d871418f3be9db2647bb249";
+
+// https://api.themoviedb.org/3/movie/674/credits?api_key=dbe82c339d871418f3be9db2647bb249
+
+function addCast() {
+    return new Promise((resolve, reject) => {
+
+        $.ajax({
+            url: castLink + movieId + apiKeyEnd,
+            type: "GET",
+            dataType: "json"
+        }).done((response1) => {
+            console.log("response1: ", response1);
+            resolve(response1);
+        });
+    });
+}
+
+module.exports = { getApiMovies, addCast };
+
 
