19baf55a35015fe81d9f5aa768f1dcf7b53bd5c1
Tyler Barnett <tyler.barnett7190@gmail.com>
2017-08-24 21:31:42 -0500

    search now produces individualized cards with images

--- javascripts/dom-builder.js
+++ javascripts/dom-builder.js
@@ -1,1 +1,16 @@
 "use strict";
+console.log ("dom-builder");
+
+let movieTemplate = require("../templates/movie-template.hbs");
+
+function populatePage(arrayofMovies) {
+	console.log ("arRaY", arrayofMovies.results);
+    let ourDiv = document.getElementById("forHandlebarsInsert");
+    ourDiv.innerHTML = movieTemplate(arrayofMovies.results);
+    // $("#attraction-column").append(newDiv);
+}
+
+module.exports = {populatePage};
+
+
+//SEND OBJECTS TO HANDLEBARS AND PRINT TO PAGE
