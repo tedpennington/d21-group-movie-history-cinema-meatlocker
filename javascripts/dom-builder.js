2238e46b561c2d6e28d4507f4afebb2cdb4f524d
jordanberghofer <jordan.berghofer@gmail.com>
2017-08-28 16:11:10 -0500

    initial commit; changed elements in index and main slider and ratings

--- javascripts/dom-builder.js
+++ javascripts/dom-builder.js
@@ -21,8 +21,20 @@ function populatePageAfterTracked(arrayOfMovies) {
 	// console.log ("arRaY", arrayOfMovies);
     let ourDiv = document.getElementById("forHandlebarsInsert");
     ourDiv.innerHTML = templateAfterTracked(arrayOfMovies);
+    $(".rateYo").rateYo({
+    numStars: 10,
+    maxValue: 10,
+    rating: "0",
+    starWidth: "25px",
+    fullStar: true
+  })
+    .on("rateyo.set", function (e, data) {
+        console.log("The rating is set to " + data.rating + "!");
+    });
+
     // $("#attraction-column").append(newDiv);
 }
+
 
 module.exports = {populatePageBeforeTracked, populatePageAfterTracked};
 
--- javascripts/fb-getter.js
+++ javascripts/fb-getter.js
@@ -1,12 +1,12 @@
 "use strict";
 
 function getKey() {
-    return {
-        apiKey: "AIzaSyDp-kuGjJGUkOAwZyoLqqTs92x-N__B6Fc",
-        authDomain: "movie-history-d6e5f.firebaseapp.com",
-        databaseURL: "https://movie-history-d6e5f.firebaseio.com/"
+  return {
+      
+    apiKey: "AIzaSyCtnoIjyR1--BXRzX0zW2aNLMzQbp_EwxU",
+    authDomain: "data-explosion.firebaseapp.com",
+    databaseURL: "https://data-explosion.firebaseio.com"
 
-    };
+  };
 }
-
 module.exports = getKey;
