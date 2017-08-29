36fcbb8614dd80fe01246b6809f58bcb3436541a
Hiatt Collins <hiatt.collins.github@gmail.com>
2017-08-29 10:00:29 -0500

    disconnected auto cast calls, started work on modal, modified information pulled into movie object array

--- javascripts/fb-getter.js
+++ javascripts/fb-getter.js
@@ -5,7 +5,6 @@ function getKey() {
   return {
       
     apiKey: "AIzaSyCtnoIjyR1--BXRzX0zW2aNLMzQbp_EwxU",
-
     authDomain: "data-explosion.firebaseapp.com",
     databaseURL: "https://data-explosion.firebaseio.com"
 
